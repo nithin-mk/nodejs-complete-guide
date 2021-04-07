b3d18f6744fd1b312e84c05b7412eed1ebd4f635
Nithin M.K <nithinmkurien@gmail.com>
2021-04-07 13:12:17 +0530

    05-fixed-missing-head-tag

--- routes.js
+++ routes.js
@@ -5,13 +5,14 @@ const requestHandler = (req, res) => {
   const method = req.method;
   if (url === '/') {
     res.write('<html>');
-    res.write('<head><title>Enter Message</title><head>');
+    res.write('<head><title>Enter Message</title></head>');
     res.write(
       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
     res.write('</html>');
     return res.end();
   }
+  
   if (url === '/message' && method === 'POST') {
     const body = [];
     req.on('data', chunk => {
