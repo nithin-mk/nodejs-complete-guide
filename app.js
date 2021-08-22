2f99d8731e98ad7677e19a85bc7a29b953f2b4ae
Nithin M.K <nithinmkurien@gmail.com>
2021-08-22 14:32:07 +0530

    assignment-handlebars-solution

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
 const users = [];
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs({ defaultLayout: 'main-layout', extname: 'hbs' }));
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,7 +17,11 @@ app.get('/', (req, res, next) => {
 });
 
 app.get('/users', (req, res, next) => {
-  res.render('users', { pageTitle: 'User', users: users });
+  res.render('users', {
+    pageTitle: 'User',
+    users: users,
+    hasUsers: users.length > 0
+  });
 });
 
 app.post('/add-user', (req, res, next) => {
