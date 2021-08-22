7b5b47c2277e4186c2dee6aa7eeaee693a56db7c
Nithin M.K <nithinmkurien@gmail.com>
2021-08-22 14:39:38 +0530

    assignment-ejs-and-final-solution

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const users = [];
 
 app.engine('hbs', expressHbs({ defaultLayout: 'main-layout', extname: 'hbs' }));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
