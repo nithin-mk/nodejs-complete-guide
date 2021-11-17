f5b913518e83a6e3c2d21d27ee56907fd0d735a7
Nithin M.K <nithinmkurien@gmail.com>
2021-11-17 13:18:54 +0530

    03-inserting-data

--- models/product.js
+++ models/product.js
@@ -12,18 +12,17 @@ module.exports = class Product {
   }
 
   save() {
-   
+    return db.execute(
+      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
   }
 
-  static deleteById(id) {
-    
-  }
+  static deleteById(id) {}
 
   static fetchAll() {
     return db.execute('SELECT * FROM products');
   }
 
-  static findById(id) {
-    
-  }
+  static findById(id) {}
 };
