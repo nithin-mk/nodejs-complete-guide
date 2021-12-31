e74ad26ea3b9c03cc79128a894388ba7dd44b942
Nithin M.K <nithinmkurien@gmail.com>
2021-12-31 13:20:52 +0530

    01-using-the-database-connection

--- util/database.js
+++ util/database.js
@@ -1,8 +1,17 @@
-const {Sequelize} = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = callback => {
+  MongoClient.connect(
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0.kkn2g.mongodb.net/test?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      console.log('Connected!');
+      callback(client);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
