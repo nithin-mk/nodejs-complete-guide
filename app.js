46237cf32b8af385376a5e1211d69ff76ecb82d7
Nithin M.K <nithinmkurien@gmail.com>
2022-07-09 12:01:49 +0530

    Fix MongoDB URI

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0.kkn2g.mongodb.net/shop';
+  'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0.kkn2g.mongodb.net/shop?retryWrites=true&w=majority';
 
 const app = express();
 const store = new MongoDBStore({
