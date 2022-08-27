0e8ff06689970a13a08e19efe848d2d5f43e5c59
Nithin M.K <nithinmkurien@gmail.com>
2022-08-27 18:31:13 +0530

    01-backend-establishing-a-connection-from-the-client

--- app.js
+++ app.js
@@ -64,6 +64,10 @@ mongoose
     'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0.kkn2g.mongodb.net/messages?retryWrites=true&w=majority'
   )
   .then(result => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('socket.io')(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
   })
   .catch(err => console.log(err));
