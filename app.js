6b93d9f9cadc7d1955d2e5d7cf4ba89fedabcc73
Nithin M.K <nithinmkurien@gmail.com>
2022-09-12 15:59:09 +0530

    04-backend-adding-login-functionality

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const graphqlHttp = require('express-graphql');
 
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
 
 const app = express();
 
@@ -51,6 +52,8 @@ app.use((req, res, next) => {
   }
   next();
 });
+
+app.use(auth);
 
 app.use(
   '/graphql',
