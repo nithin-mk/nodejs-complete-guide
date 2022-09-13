9150f1da77d369fab5702b063b4c6ee200687a45
Nithin M.K <nithinmkurien@gmail.com>
2022-09-13 12:22:09 +0530

    05-adding-a-create-post-mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -100,6 +100,7 @@ module.exports = {
     });
     const createdPost = await post.save();
     user.posts.push(createdPost);
+    await user.save();
     return {
       ...createdPost._doc,
       _id: createdPost._id.toString(),
