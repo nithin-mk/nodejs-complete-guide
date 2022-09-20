e0de7cb4b810e946f40da4e8549091395d1f2152
Nithin M.K <nithinmkurien@gmail.com>
2022-09-20 12:22:08 +0530

    10-backend-deleting-posts

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const fs = require('fs');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,6 +9,7 @@ const graphqlHttp = require('express-graphql');
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
 const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
 const app = express();
 
@@ -106,7 +106,4 @@ mongoose
   })
   .catch(err => console.log(err));
 
-const clearImage = filePath => {
-  filePath = path.join(__dirname, '..', filePath);
-  fs.unlink(filePath, err => console.log(err));
-};
+
