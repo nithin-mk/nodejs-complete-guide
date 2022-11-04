d2750b0219c52af90fe1c377253fbfcb94664068
Nithin M.K <nithinmkurien@gmail.com>
2022-11-04 16:02:16 +0530

    04-promises

--- response-handler.js
+++ response-handler.js
@@ -1,5 +1,5 @@
-// const fs = require('fs');
-import fs from 'fs';
+// const fs = require('fs/promises');
+import fs from 'fs/promises';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
@@ -7,10 +7,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const resHandler = (req, res, next) => {
-  // fs.readFile('my-page.html', 'utf8', (err, data) => {
-  //   res.send(data);
-  // });
-  res.sendFile(path.join(__dirname, 'my-page.html'));
+  fs.readFile('my-page.html', 'utf8')
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  // res.sendFile(path.join(__dirname, 'my-page.html'));
 };
 
 // module.exports = resHandler;
