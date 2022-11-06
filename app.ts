13b18a5f219b6b7cf3ec57633911f64c600c4c9a
Nithin M.K <nithinmkurien@gmail.com>
2022-11-06 14:05:23 +0530

    07-generic-types-generics

--- app.ts
+++ app.ts
@@ -2,10 +2,18 @@ const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2') as HTMLInputElement;
 const buttonElement = document.querySelector('button')!;
 
-const numResults: number[]  = [];
+const numResults: Array<number> = [];
 const textResults: string[] = [];
 
-function add(num1: number | string, num2: number | string) {
+type NumOrString = number | string;
+type Result = { val: number; timestamp: Date };
+
+interface ResultObj {
+  val: number;
+  timestamp: Date;
+}
+
+function add(num1: NumOrString, num2: NumOrString) {
   if (typeof num1 === 'number' && typeof num2 === 'number') {
     return num1 + num2;
   } else if (typeof num1 === 'string' && typeof num2 === 'string') {
@@ -14,7 +22,7 @@ function add(num1: number | string, num2: number | string) {
   return +num1 + +num2;
 }
 
-function printResult(resultObj: { val: number; timestamp: Date }) {
+function printResult(resultObj: ResultObj) {
   console.log(resultObj.val);
 }
 
@@ -27,4 +35,14 @@ buttonElement.addEventListener('click', () => {
   textResults.push(stringResult as string);
   printResult({ val: result as number, timestamp: new Date() });
   console.log(numResults, textResults);
+});
+
+const myPromise = new Promise<string>((resolve, reject) => {
+  setTimeout(() => {
+    resolve('It worked!');
+  }, 1000);
+});
+
+myPromise.then((result) => {
+  console.log(result.split('w'));
 });
