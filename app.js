01dc03d4ca0f70c235a4426dbceeb9f1b98c3f97
Nithin M.K <nithinmkurien@gmail.com>
2022-11-11 21:39:04 +0530

    04-node-server

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const fs = require('fs').promises;
+const http = require('http');
 
-const text = 'This is a test - and it should be stored in a file!';
-
-fs.writeFile('node-message.txt', text).then(() => {
-  console.log('Wrote file!');
+const server = http.createServer((req, res) => {
+  res.end('Hello World (from Node!)');
 });
+
+server.listen(3000);
