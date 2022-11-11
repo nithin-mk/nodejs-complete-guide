204a15bbc1907bd7697df0790675b74e27387bbf
Nithin M.K <nithinmkurien@gmail.com>
2022-11-11 21:20:44 +0530

    03-basic-deno-server

--- app.ts
+++ app.ts
@@ -1,8 +1,7 @@
-const text = 'This is a test - and it should be stored in a file!';
+import { serve } from "https://deno.land/std@0.104.0/http/server.ts";
 
-const encoder = new TextEncoder();
-const data = encoder.encode(text);
+const server = serve({ port: 3000 });
 
-Deno.writeFile('message.txt', data).then(() => {
-  console.log('Wrote to file!');
-});
+for await (const req of server) {
+  req.respond({ body: "Hello World\n" });
+}
