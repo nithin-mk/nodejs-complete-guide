c4984807a791de0f46db653544e5dc571c70dba2
Nithin M.K <nithinmkurien@gmail.com>
2022-11-14 12:46:41 +0530

    02-cors

--- deno/app.ts
+++ deno/app.ts
@@ -9,7 +9,14 @@ app.use(async (ctx, next) => {
   await next();
 });
 
+app.use(async (ctx, next) => {
+  ctx.response.headers.set('Access-Control-Allow-Origin', '*');
+  ctx.response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  ctx.response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+  await next();
+});
+
 app.use(todosRoutes.routes());
 app.use(todosRoutes.allowedMethods());
 
-await app.listen({ port: 3000 });
+await app.listen({ port: 8000 });
